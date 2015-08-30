21f1d7a436b55d3d9de06c06f75fdc25d57d23c1
cotrpepe <cotrpepe@gmail.com>
2015-08-30 10:33:34 +0900

    Cache a DOM element

--- src/renderer/main-window.js
+++ src/renderer/main-window.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var ss = require('./string-style.js');
+var dom = require('./dom.js');
 var string = require('./string.js');
 
 var updateHTML = function(fontSize, text) {
-	var strLenElement = document.getElementById('strLen');
+	var strLenElement = dom.getCachedElement('strLen');
 	strLenElement.style.fontSize = fontSize;
 	strLenElement.innerText = text;
 };
